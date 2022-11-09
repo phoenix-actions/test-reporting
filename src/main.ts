6aea17469161e0cfad9ab077e75326f46ec1c5e3
Ian Moroney <ian.moroney@firstlight.games>
2022-11-09 08:49:44 +0000

    update actions/core and fix linting errors

--- src/main.ts
+++ src/main.ts
@@ -236,6 +236,7 @@ class TestReporter {
         core.info(`Check run create response: ${resp.status}`)
         core.info(`Check run URL: ${resp.data.url}`)
         core.info(`Check run HTML: ${resp.data.html_url}`)
+        core.setOutput(Outputs.runHtmlUrl, `${resp.data.html_url}`)
         break
       }
       case 'step-summary': {
